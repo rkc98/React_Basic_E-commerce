df3665c57afb0c165d39338e8e6868afc010baa4
Rahul <rkc.98@outlook.com>
2021-10-25 14:05:29 +0530

    added annoucement

---
+++ src/components/Annocuments.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+const Annocuments = () => {
+    return (
+        <Container>
+            Free shipping on orders above Rs 500
+        </Container>
+    )
+}
+
+const Container=styled.div`
+height: 30px;
+ background-color: teal;
+ color: white;
+ display: flex;
+ align-items: center;
+ justify-content: center;
+
+`
+
+
+export default Annocuments
